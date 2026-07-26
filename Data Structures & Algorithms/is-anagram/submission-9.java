68c83b4aed0c00d897685c5b4d4b4cc1badd1c80
Makay200 <98334705+Makay200@users.noreply.github.com>
2026-07-26 09:46:07 +0100

    Bulk sync: 13 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] tchar = t.toCharArray();
+        char[] schar = s.toCharArray();
+
+        Arrays.sort(tchar);
+        Arrays.sort(schar);
+
+        return Arrays.equals(tchar, schar);
+    }
+}
